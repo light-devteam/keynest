308ed497cf5ec4d575d15ce770e54bfe31ab6265
rTiRe <zientenin@mail.ru>
2026-04-26 01:19:36 +0300

    feat: add logger

---
+++ Makefile
@@ -0,0 +1,2 @@
+run:
+	go run ./cmd/keynest/main.go --config ./config/local.yaml
---
+++ internal/logger/logger.go
@@ -0,0 +1,40 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func SetupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(
+				os.Stdout,
+				&slog.HandlerOptions{Level: slog.LevelDebug},
+			),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewTextHandler(
+				os.Stdout,
+				&slog.HandlerOptions{Level: slog.LevelInfo},
+			),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewTextHandler(
+				os.Stdout,
+				&slog.HandlerOptions{Level: slog.LevelWarn},
+			),
+		)
+	}
+	return log
+}
